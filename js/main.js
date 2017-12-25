19f062ce269687f59632644ca6a3f66126513053
Melissa Thompson <mthompson828@gmail.com>
2017-12-24 21:44:53 -0500

    fixed hover effects for mobile

--- js/main.js
+++ js/main.js
@@ -5,13 +5,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	let callToAction = document.querySelector('#call-to-action');
 
 	callToAction.addEventListener('click', function(){
-		smoothScroll('work');
+		smoothScroll('#work');
 	})
 
-	function smoothScroll(elementId) {
-	    var MIN_PIXELS_PER_STEP = 0;
-	    var MAX_SCROLL_STEPS = 22;
-	    var target = document.getElementById(elementId);
+	function smoothScroll() {
+    var MIN_PIXELS_PER_STEP = 1;
+    var MAX_SCROLL_STEPS = 15;
+    var target = document.getElementById('work');
+    var scrollContainer = target;
+    do {
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do {
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    var pixelsPerStep = Math.max(MIN_PIXELS_PER_STEP,
+                                 (targetY - scrollContainer.scrollTop) / MAX_SCROLL_STEPS);
+
+    var stepFunc = function() {
+        scrollContainer.scrollTop =
+            Math.min(targetY, pixelsPerStep + scrollContainer.scrollTop);
+
+        if (scrollContainer.scrollTop >= targetY) {
+            return;
+        }
+
+        window.requestAnimationFrame(stepFunc);
+    };
+
+    window.requestAnimationFrame(stepFunc);
+}
+	    var target = document.getElementById('elementId');
 	    var scrollContainer = target;
 	    do {
 	        scrollContainer = scrollContainer.parentNode;
@@ -40,6 +70,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	    };
 
 	    window.requestAnimationFrame(stepFunc);
-	}
-
-});
+	});
